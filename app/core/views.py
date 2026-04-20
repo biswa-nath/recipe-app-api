14012b2356257d9786549745a3d7351a75e9c678
Biswanath <p_biswanath@yahoo.co.in>
2026-04-20 12:54:38 +0530

    Fixes for review comments

--- app/core/views.py
+++ app/core/views.py
@@ -3,22 +3,9 @@ Core views for app.
 """
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework import status
-
-from user.serializers import UserSerializer
-
 
 @api_view(['GET'])
 def health_check(request):
     """Returns successful response."""
     return Response({'healthy': True})
 
-
-@api_view(['POST'])
-def create_user(request):
-    """Creates a new user."""
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
